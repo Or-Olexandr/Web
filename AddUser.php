9ff311cd28526180f7bb4355cc1b01e41d6c5b07
OrOle <or.olexandr1@gmail.com>
2019-10-10 01:59:46 +0300

    create design for add

--- AddUser.php
+++ AddUser.php
@@ -7,6 +7,8 @@ session_start();
 
 <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/materialize.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,24 +19,30 @@ maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/materialize.min.css">
-<style>
-.container{
-	width: 400px;
-}
-</style>
+
 </head>
 <body style="padding-top: 3rem;">
-<div class="container">
 <form action="insert.php" method="post">
-Name: <input type="text" name="name"><br>
-Surname:<input type="text" name="surname"><br>
-Role:     <select name="Roles" >
+    <div class="form-group">
+        <label for="exampleInputName">Name</label>
+        <input type="text" class="form-control" id="exampleInputName" name="name" placeholder="Enter name">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputSurname">Surname</label>
+        <input type="text" class="form-control" id="exampleInputName" name="surname" placeholder="Enter Surname">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+    </div>
+        <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" name="Roles">
+        <option selected>Role</option>
         <option value="1">Admin</option>
         <option value="2">User</option>
     </select><br>
-Password : <input type="password" name="password"><br>
-<input type="submit" value="Submit" class="btn">
+    <h1></h1>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-</div>
 </body>
 </html>
