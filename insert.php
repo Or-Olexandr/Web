a3b82c5c3cfa4dbb8f13d4be3f7c2aaa157ab0df
OrOle <or.olexandr1@gmail.com>
2019-10-19 23:01:52 +0300

    Fix upload image

--- insert.php
+++ insert.php
@@ -39,18 +39,25 @@ if(isset($_POST["submit"])) {
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    $_SESSION["error"] = "Sorry, file already exists.";
+    header('Location: ' . $newURL);
+    header('Location: AddUser.php');
+    die();
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    $_SESSION["error"] = "Sorry, your file is too large.";
+    header('Location: ' . $newURL);
+    header('Location: AddUser.php');
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $_SESSION["error"] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    header('Location: ' . $newURL);
+    header('Location: AddUser.php');
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -61,7 +68,9 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        $_SESSION["error"] =  "Sorry, there was an error uploading your file.";
+        header('Location: ' . $newURL);
+        header('Location: AddUser.php');
     }
 }
     mysqli_query($conn, "INSERT INTO users (first_name, last_name, password, image, id_role)
